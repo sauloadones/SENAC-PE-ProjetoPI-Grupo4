6c18f957fde6e43e38af65f9286fb03afa660a16
sauloadones <sauloadonesg@gmail.com>
2025-04-29 12:50:20 -0300

    Feat - Adicionado novas entidades no banco

--- app/src/main/java/com/example/appsenkaspi/FuncionarioViewModel.kt
+++ app/src/main/java/com/example/appsenkaspi/FuncionarioViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.appsenkaspi.Converters.Cargo
 import kotlinx.coroutines.launch
 
 
@@ -13,47 +14,49 @@ class FuncionarioViewModel(application: Application) : AndroidViewModel(applicat
         FuncionarioEntity(
             nomeCompleto = "Ana Beatriz Souza",
             email = "ana.souza@example.com",
-            cargo = "Engenheira Eletricista",
+            cargo = Cargo.COORDENADOR,
             fotoPerfil = "https://i.pravatar.cc/150?img=1",
             nomeUsuario = "ana.souza",
-            senha = "senha123"
+            senha = "senha123",
+            idAcesso = 1
         ),
         FuncionarioEntity(
             nomeCompleto = "Carlos Eduardo Silva",
             email = "carlos.silva@example.com",
-            cargo = "Técnico em TI",
+            cargo = Cargo.GESTOR,
             fotoPerfil = "https://i.pravatar.cc/150?img=2",
             nomeUsuario = "carlos.silva",
-            senha = "senha123"
+            senha = "senha123",
+            idAcesso = 2
         ),
         FuncionarioEntity(
             nomeCompleto = "Fernanda Oliveira",
             email = "fernanda.oliveira@example.com",
-            cargo = "Analista de Dados",
+            cargo = Cargo.APOIO,
             fotoPerfil = "https://i.pravatar.cc/150?img=3",
             nomeUsuario = "fernanda.oliveira",
-            senha = "senha123"
+            senha = "senha123",
+            idAcesso = 3
         ),
         // adicione quantos quiser…
     )
-    private val dao = AppDatabase.getDatabase(application).funcionarioDao()
-    val listasFuncionarios = dao.listarTodosFuncionarios()
 
-    init {
-        // Na primeira vez que abrir, popula se estiver vazio
-        viewModelScope.launch {
-            val atuais = listasFuncionarios.value
-            if (atuais.isNullOrEmpty()) {
-                dao.inserirTodos(funcionariosTeste)
-            }
-        }
-    }
 
 
     private val funcionarioDao = AppDatabase.getDatabase(application).funcionarioDao()
     val listaFuncionarios: LiveData<List<FuncionarioEntity>> =
         funcionarioDao.listarTodosFuncionarios()
 
+    init {
+        // Na primeira vez que abrir, popula se estiver vazio
+        viewModelScope.launch {
+            val atuais = listasFuncionarios.value
+            if (atuais.isNullOrEmpty()) {
+                funcionarioDao.inserirTodos(funcionariosTeste)
+            }
+        }
+    }
+    val listasFuncionarios = funcionarioDao.listarTodosFuncionarios()
     fun inserir(funcionario: FuncionarioEntity) = viewModelScope.launch {
         funcionarioDao.inserirFuncionario(funcionario)
     }
--- app/src/main/java/com/example/appsenkaspi/PilarEntity.kt
+++ app/src/main/java/com/example/appsenkaspi/PilarEntity.kt
@@ -2,6 +2,9 @@ package com.example.appsenkaspi
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
+import com.example.appsenkaspi.Converters.Cargo
+import com.example.appsenkaspi.Converters.StatusPilar
 import java.util.Date
 
 @Entity(tableName = "pilares")
@@ -10,5 +13,10 @@ data class PilarEntity(
     val nome: String,
     val descricao: String,
     val dataInicio: Date,
-    val dataPrazo: Date
+    val dataPrazo: Date,
+    val dataCriacao: Date,
+
+    @ColumnInfo(name = "status") val status: StatusPilar,
+
+
 )
