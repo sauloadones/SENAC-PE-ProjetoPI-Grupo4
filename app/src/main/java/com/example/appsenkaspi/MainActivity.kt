a13a490f42c31045a20e0114d809a0ea4acf667c
Andrey Silva <andreyrdh@gmail.com>
2025-05-01 03:02:29 -0300

    Primeira parte
    Adicionando animação na navegation bar e estilizando o app

--- app/src/main/java/com/example/appsenkaspi/MainActivity.kt
+++ app/src/main/java/com/example/appsenkaspi/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.appsenkaspi
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Define a cor da status bar
+        window.statusBarColor = ContextCompat.getColor(this, R.color.graycont)
+
+        // Deixa os ícones brancos (ideal para fundo escuro)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
 
         // Inicializa o botão e define o clique para abrir o popup
         val botaoLogin = findViewById<Button>(R.id.loginButtonPrincipal)
